f8ee0470c98ef8cc6da66c28050942adb295ae7e
Leah <Lebrra@users.noreply.github.com>
2020-02-01 14:39:58 -0600

    started more cards

---
+++ CastleRepair/Assets/Scripts/CardTypes/MultiShot.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MultiShot : Card
+{
+    public override void setCardActive(GameObject myPlayer)
+    {
+        myPlayer.GetComponent<PlayerMove>().multiShot = true;
+        isActive = true;
+    }
+
+    public override void setCardUnactivate(GameObject myPlayer)
+    {
+        if (!isPermanent)
+        {
+            myPlayer.GetComponent<PlayerMove>().multiShot = false;
+            isActive = false;
+        }
+    }
+}
