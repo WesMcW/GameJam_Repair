5074faad23da0b34cb2082d7d53be7afe4342fe7
zombykillr123 <jaklein25@gmail.com>
2020-02-01 11:59:41 -0600

    Update PlayerMove.cs

--- CastleRepair/Assets/Scripts/PlayerMove.cs
+++ CastleRepair/Assets/Scripts/PlayerMove.cs
@@ -86,7 +86,6 @@ public class PlayerMove : MonoBehaviour {
             }
         }
 
-
         // If they are dead, freeze them (movement speed set to 0 if we want to switch to dead sprite)
         if (isDead == true)
         {
@@ -132,5 +131,4 @@ public class PlayerMove : MonoBehaviour {
             Physics2D.IgnoreCollision(clone.GetComponent<BoxCollider2D>(), myCollider);
         }
     }
-
 }
