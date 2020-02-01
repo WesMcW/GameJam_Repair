c61faacb659e030b24b90ba411323e31fd7c67c8
mcclendokj11 <47314781+mcclendokj11@users.noreply.github.com>
2020-02-01 17:02:54 -0600

    Player count screen

---
+++ CastleRepair/Assets/Scripts/Playbutton.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Playbutton : MonoBehaviour
+{
+    public GameObject playerCount;
+    //public bool playPressed;
+
+    private void Start()
+    {
+        playerCount.SetActive(false);
+        //playPressed = false;
+    }
+
+    public void ChangeScreen()
+    {
+        playerCount.SetActive(true);
+    }
+   
+}
