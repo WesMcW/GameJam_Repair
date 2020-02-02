f9c976616fa7db13d28f3595f0978a3d3deeb684
Leah <Lebrra@users.noreply.github.com>
2020-02-01 22:12:21 -0600

    started making new cards

---
+++ CastleRepair/Assets/Scripts/CardTypes/Penalty_Slow.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Penalty_Slow : Card
+{
+    public override void setCardActive(GameObject myPlayer)
+    {
+        int target = Random.Range(0, PlayerPrefs.GetInt("PlayerCount"));
+        myPlayer.GetComponent<PlayerMove>().pierceShot = true;
+        isActive = true;
+    }
+
+    public override void setCardUnactivate(GameObject myPlayer)
+    {
+        if (!isPermanent)
+        {
+            myPlayer.GetComponent<PlayerMove>().pierceShot = false;
+            isActive = false;
+        }
+    }
+}
--- CastleRepair/Assets/Scripts/CardTypes/Penalty_TakePoint.cs
+++ CastleRepair/Assets/Scripts/CardTypes/Penalty_TakePoint.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DoublePoints : Card
+public class Penalty_TakePoint : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
